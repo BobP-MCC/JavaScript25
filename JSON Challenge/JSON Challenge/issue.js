0ca0acd08a4695fd61b2d9d26ce423324beb13f8
BobP-MCC <rpeck1029@mchenry.edu>
2025-10-28 23:12:13 -0500

    Add files via upload

---
+++ JSON Challenge/JSON Challenge/issue.js
@@ -0,0 +1,16 @@
+const queue = '[{"id":"394","title":"Eat your veggies"},{"id":"378","title":"Every step counts"},{"id":"406","title":"Giving Back"}]';
+
+// Enter code to destringify the `queue` variable here:
+
+const queueObj = JSON.parse(queue);
+
+console.log(queueObj);
+
+
+const item = '{"id":"406","title":"Giving Back"}';
+
+// Enter code to destringify the `item` variable here:
+
+const itemObj = JSON.parse(item);
+
+console.log(itemObj);
